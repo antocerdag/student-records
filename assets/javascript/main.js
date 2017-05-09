2abb20eb5a4b04be72fc92fc5c130c40ea04d766
Camila Sáez <cfsaezbravo@gmail.com>
2017-05-09 12:06:09 -0300

    Changing elements

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,10 @@
-main.js
+var productos = [];
+
+var addStudent = document.getElementById('nuevacompra');
+addCompra.onclick = function() {
+  var compra = prompt('Ingrese su nueva compra');
+  var cantidad = prompt('Ingrese la cantidad');
+  var product  = new Producto (compra, cantidad);
+  productos.push(product);
+  printHTML(product.toHTML());
+};
