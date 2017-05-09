aae9fed5f10fa2b6f5545e12e1b52bb8f1a60485
Camila Sáez <cfsaezbravo@gmail.com>
2017-05-09 18:55:24 -0300

    Update main.js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,43 +1,36 @@
+var arrayEstudiante = [];
 
-//suponiendo que el constructor esta hecho, se agrega funcionalidad del boton printAll
-
-//suponemos a priori que en el onclick del botón PrintAll, se encuentra la función imprimirTodo()
-
-//object nuevaEstudiante
-
-/*function AddStudent(name, techSkills, lifePoints, status) {
+function Estudiante(name,techSkill,lifePoints,status){
   this.name = name;
-  this.techSkills = techSkills ;
+  this.techSkill = techSkill;
   this.lifePoints = lifePoints;
   this.status = status;
-}*/
+};
 
-var arrayEstudiante = [];
-var memoria = 
-
-var addEstudiante = document.getElementById("nuevoestudiante");
-addEstudiante.onclick = function() {
-  var name = prompt("Ingrese nuevo estudiante");
+function addStudent(){
+  
+  var name = prompt("Ingrese nuevo estudiante").toLowerCase();
   var techSkill = prompt("Ingrese porcentaje técnico");
-  var lifePoint = prompt("Ingrese habilidades socioemocionales");
+  var lifePoints = prompt("Ingrese habilidades socioemocionales");
   var status = prompt("Ingrese status");
-  //var student = new AddStudent (name,techSkills,lifePoints,status);
-  var Student = {};
-	Student["name"] = name;
-	Student["techSkill"] = techSkill;
-	Student["lifePoint"] = lifePoint;
-	Student["status"] = status;
-	
-  arrayEstudiante.push(Student);
+  var student = new Estudiante(name,techSkill,lifePoints,status);
+
+  arrayEstudiante.push(student);
+  print(name,techSkill,lifePoints,status);
+};
+
+function print(name,techSkill,lifePoints,status){
+  var value = document.getElementById("informacion");
+
+  value.innerHTML = name + "<br>" + "Tech Skills: " + techSkill + "%" + "<br>" + "Life Points: " + lifePoints + "%" + "<br>" + status;
 };
 
 function imprimirTodo(nuevaEstudiante){
-	arrayEstudiante.push(nuevaEstudiante);
-	var ficha = document.getElementById("botonprint");
-	arrayEstudiante.forEach(function(element){
-		ficha.innerHTML = "<div>" + "Nombre: " + element.nombre + "<br>" + "Tech Skills: " + element.techSkill + "%" + "<br>" + "Life Skills: " + element.lifeSkill + "%" + "Status: " + element.status + "</div>"
-	});
-}; 
+  
+  arrayEstudiante.forEach(function(el){
+    resultado.innerHTML += el.name + "<br>" + "Tech Skills: " + el.techSkill + "%" + "<br>" + "Life Points: " + el.lifePoints + "%" + "<br>" + el.status;
+  })
+  };
 
  
 
