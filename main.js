1eb74f97a3d3a0cdbee29126a41c25ad25c078a1
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-05-09 12:26:14 -0300

    se cambia nombre de archivo a main.js. Se define el nombre de la función que imprime todo

---
+++ main.js
@@ -0,0 +1,7 @@
+//suponiendo que el constructor esta hecho, se agrega funcionalidad del boton printAll
+
+//suponemos a priori que en el onclick del botón PrintAll, se encuentra la función imprimirTodo()
+
+function imprimirTodo(){
+	
+} 
