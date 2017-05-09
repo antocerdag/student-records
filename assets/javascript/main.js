1149f51c888ac5a6ed1dd197a44dc57e9c3c74e7
María José <mariaj.pozom@gmail.com>
2017-05-09 13:26:00 -0300

    Update main.js
    editando main

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -5,21 +5,21 @@
 
 //object nuevaEstudiante
 
-function AddStudent(name, techSkills, lifePoints, status) {
+/*function AddStudent(name, techSkills, lifePoints, status) {
   this.name = name;
   this.techSkills = techSkills ;
   this.lifePoints = lifePoints;
   this.status = status;
-}
+}*/
 
 var addEstudiante = document.getElementById("nuevoestudiante");
 addEstudiante.onclick = function() {
   var name = prompt("Ingrese nuevo estudiante");
-  var techSkills = prompt("Ingrese porcentaje técnico");
-  var lifePoints = prompt("Ingrese habilidades socioemocionales");
+  var techSkill = prompt("Ingrese porcentaje técnico");
+  var lifePoint = prompt("Ingrese habilidades socioemocionales");
   var status = prompt("Ingrese status");
-  var student = new AddStudent (name,techSkills,lifePoints,status);
-  var prueba = [];
+  //var student = new AddStudent (name,techSkills,lifePoints,status);
+  var prueba = {};
   prueba.push(student);
 };
 
@@ -28,7 +28,7 @@ function imprimirTodo(nuevaEstudiante){
 	arrayEstudiante.push(nuevaEstudiante);
 	var ficha = document.getElementById("botonprint");
 	arrayEstudiante.forEach(function(element){
-		ficha.innerHTML = "<div>" + "Nombre: " + element.nombre + "<br>" + "Tech Skills: " + element.techSkill + "%" + "<br>" + "Life Skills: " + element.lifeSkill + "%" + "Status: " + element.actividad + "</div>"
+		ficha.innerHTML = "<div>" + "Nombre: " + element.nombre + "<br>" + "Tech Skills: " + element.techSkill + "%" + "<br>" + "Life Skills: " + element.lifeSkill + "%" + "Status: " + element.status + "</div>"
 	});
 }; 
 
